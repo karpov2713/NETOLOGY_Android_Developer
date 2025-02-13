9da5d4f3d9516a6c927e5df78f4b37dd30fa16d1
karpov2713 <karpov2713@gmail.com>
2025-02-14 02:38:19 +0300

    ООП Поля класса

--- ClassStructure/src/Main.java
+++ ClassStructure/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        String name = "Petya";
+//        int age = 8;
+//        int rating = 3;
+
+        Singer singer = new Singer();
+        singer.name = "Petya";
+        singer.age = 8;
+        singer.rating = 3;
+        f(singer);
+
+        A.x = 5; // При обращении к статическому полю не требуется объявления экземпляра объекта.
     }
+
+    public static void f(Singer singer){
+        System.out.println("Name: " + singer.name + ", Age: " + singer.age + ", Rating: " + singer.rating + ".");
+    }
+
 }
---
+++ ClassStructure/src/Singer.java
@@ -0,0 +1,5 @@
+public class Singer {
+    public String name;
+    public int age;
+    public int rating;
+}
