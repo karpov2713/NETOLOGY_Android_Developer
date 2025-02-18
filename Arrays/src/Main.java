c2ce3499d20aea5f81e3771c48ac6acc1f462558
karpov2713 <karpov2713@gmail.com>
2025-02-18 17:05:01 +0300

    Arrays 1.

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        int[] arr = new int[5];
+        arr[0] = 5;
+        System.out.println(arr[0]);
+        System.out.println(arr[4]);
+        System.out.println(Arrays.toString(arr));
+
+        // Заполняем ячейки массива
+        arr[1] = 3;
+        arr[2] = 15;
+        arr[3] = 100;
+        arr[4] = 1;
+        System.out.println(Arrays.toString(arr));
+
+        int[] arr2 = {5, 3, 15, 100, 1};
+        System.out.println(Arrays.toString(arr2));
+
+        System.out.println(arr == arr2); // false
+        System.out.println(arr.equals(arr2)); // false работает как = =
+        System.out.println(Arrays.equals(arr, arr2)); // true
+
+        String[] arrStr = {"Petya", "Olga", "Tanya"};
+
+        Singer[] arrSing = {
+                new Singer("Petya", 8),
+                new Singer("Tanya", 15, 4)
+        };
     }
 }
---
+++ Arrays/src/Singer.java
@@ -0,0 +1,18 @@
+public class Singer {
+    public static int maxRating = 0;
+
+    public String name;
+    public int age;
+    public int rating;
+
+    public Singer(String name, int age, int rating) {
+        this.name = name;
+        this.age = age;
+        this.rating = rating;
+        maxRating = Math.max(maxRating,rating);
+    }
+
+    public Singer(String name, int age){
+        this(name, age, 3);
+    }
+}
