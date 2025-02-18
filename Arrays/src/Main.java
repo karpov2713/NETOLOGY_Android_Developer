fc59c9f209cee8600a1f7e8e21444928277ed736
karpov2713 <karpov2713@gmail.com>
2025-02-18 18:18:10 +0300

    Arrays 4
    Практическое задание: расчет средней температура

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
 //        int[] arr = new int[5];
 //        arr[0] = 5;
@@ -28,42 +30,74 @@ public class Main {
 //        Singer[] arrSing = {
 //                new Singer("Petya", 8),
 //                new Singer("Tanya", 15, 4)
+
 //        };
-        String[] names = {
-                "Petya",
-                "Olga",
-                "Anya",
-                "Pavel",
-                "Oleg"
-        };
+//        String[] names = {
+//                "Petya",
+//                "Olga",
+//                "Anya",
+//                "Pavel",
+//                "Oleg"
+//        };
+//
+//        for (int i = 0; i < names.length; i++) {
+//            if (!names[i].startsWith("P")) {
+//                System.out.println(names[i]);
+//            }
+//        }
+//        System.out.println("---------------------------------"); // Далее цикл foreach;
+//
+//        for (String name : names) {
+//            if (!name.startsWith("P")) {
+//                System.out.println(name);
+//            }
+//        }
+//        System.out.println("---------------------------------"); // Далее сортировка массива
+//
+//        Arrays.sort(names);
+//        System.out.println(Arrays.toString(names));
+//        System.out.println("---------------------------------");
 
-        for (int i = 0; i < names.length; i++) {
-            if (!names[i].startsWith("P")) {
-                System.out.println(names[i]);
+//        String[] names2 = names;
+//        names2[0] = null;
+//        System.out.println(Arrays.toString(names));
+//        System.out.println(Arrays.toString(names2));
+//        System.out.println("---------------------------------");
+//
+//        String[] names3 = names.clone();
+//        names3[1] = null;
+//        System.out.println(Arrays.toString(names));
+//        System.out.println(Arrays.toString(names3));
+
+        // Массивы 4 - практическое занятие
+
+        int[] temps = new int[30]; // Массив со значениями за 30 дней месяца
+        boolean[] isFilled = new boolean[30]; // все ячейки заполнены false
+        while (true) {
+            System.out.println("Введите два числа: дата температура"); // 11 31
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
             }
-        }
-        System.out.println("---------------------------------"); // Далее цикл foreach;
 
-        for (String name : names) {
-            if (!name.startsWith("P")) {
-                System.out.println(name);
+            String[] parts = input.split(" ");
+            int day = Integer.parseInt(parts[0]) - 1;
+            int t = Integer.parseInt(parts[1]);
+            temps[day] = t;
+            isFilled[day] = true;
+
+            double sum = 0;
+            for (int tt : temps) {
+                sum += tt;
             }
+            int cnt = 0;
+            for (boolean f : isFilled) {
+                if (f) {
+                    cnt++;
+                }
+            }
+            System.out.println("Средняя температура: " + (sum / cnt));
         }
-        System.out.println("---------------------------------"); // Далее сортировка массива
 
-        Arrays.sort(names);
-        System.out.println(Arrays.toString(names));
-        System.out.println("---------------------------------");
-
-        String[] names2 = names;
-        names2[0] = null;
-        System.out.println(Arrays.toString(names));
-        System.out.println(Arrays.toString(names2));
-        System.out.println("---------------------------------");
-
-        String[] names3 = names.clone();
-        names3[1] = null;
-        System.out.println(Arrays.toString(names));
-        System.out.println(Arrays.toString(names3));
     }
 }
