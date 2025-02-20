c48b0ac6bfdc822f3f8e04ce6183eb0e6461fa96
karpov2713 <karpov2713@gmail.com>
2025-02-20 19:28:34 +0300

    2. Многомерные массивы. Основы
    2. Основы работы с многомерными массивами

--- NET_MultidimArrays/src/Main.java
+++ NET_MultidimArrays/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] arr = new int[5]; // Одномерный массив
+
+        int[][] arr2 = new int[3][5];
+//        arr2[0] = new int[1];
+//        arr2[1] = new int[4];
+//        arr2[2] = new int[2];
+        arr2[1][3] = 15;
+        System.out.println(arr2[1][3]);
+
+        int[][] arr3 = {
+                new int[1],
+                new int[4],
+                new int[2]
+        };
+
+        int[][] arr4 = {
+                {1},
+                {22, 222},
+                {333, 345, 478}
+        };
+
+        System.out.println(Arrays.deepToString(arr2));
+        System.out.println(Arrays.deepToString(arr3));
+        System.out.println(Arrays.deepToString(arr4));
+
+        for (int i = 0; i < arr4.length; i++) {
+            //int[] tmp = arr4[i];
+            for (int j = 0; j < arr4[i].length; j++) {
+                System.out.print(arr4[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int[][][] arr5 = new int[10][20][5]; // Трехмерный массив
+        //System.out.println(arr5[100]);
+        //System.out.println(Arrays.deepToString(arr5));
     }
 }
