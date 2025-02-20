860ae99ba6c8913b7916706bb881005540bce0e4
karpov2713 <karpov2713@gmail.com>
2025-02-21 01:18:05 +0300

    HW_MultiArrays. Task 2.
    Решение ДЗ, задание 2.

---
+++ NET_HW_MultiArrays/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static final int SIZE = 8;
+
+    public static void main(String[] args) {
+        int[][] colors = new int[SIZE][SIZE];
+        int[][] rotatedColors90 = new int[SIZE][SIZE];
+        int[][] rotatedColors180 = new int[SIZE][SIZE];
+        int[][] rotatedColors270 = new int[SIZE][SIZE];
+
+
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                colors[i][j] = random.nextInt(256);
+                System.out.format("%4d", colors[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите угол 90, 180 или 270 поворота матрицы: ");
+        String input = scanner.nextLine();
+        System.out.println();
+
+        switch (input) {
+
+            // Поворот матрицы на 90 градусов по часовой стрелке
+            case "90":
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    rotatedColors90[i][j] = colors[SIZE - 1 - j][i];
+                    System.out.format("%4d", rotatedColors90[i][j]);
+                }
+                System.out.println();
+            }
+            break;
+
+            // Поворот матрицы на 180 градусов
+            case "180":
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    rotatedColors180[i][j] = colors[SIZE - 1 - i][SIZE - 1 - j];
+                    System.out.format("%4d", rotatedColors180[i][j]);
+                }
+                System.out.println();
+            }
+            break;
+
+            // Поворот матрицы на 270 градусов по часовой или 90 градусов против часовой стрелки
+            case "270":
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    rotatedColors180[i][j] = colors[j][SIZE - 1 - i];
+                    System.out.format("%4d", rotatedColors180[i][j]);
+                }
+                System.out.println();
+            }
+            break;
+        }
+    }
+}
