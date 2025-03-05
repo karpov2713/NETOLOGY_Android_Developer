d807337cdb76c530ddc8367ad4e2d81fedada6ba
karpov2713 <karpov2713@gmail.com>
2025-03-05 13:34:38 +0300

    Полиморфизм. Тест. Начало

--- NET_OOP_Basics/POLIMORF/src/Main.java
+++ NET_OOP_Basics/POLIMORF/src/Main.java
@@ -1,20 +1,37 @@
 public class Main {
-    public static void main(String[] args) {
-        Singer singer = new Singer("Tanya", 15, 4);
+    public static void main(String[] args) throws Exception {
+//        Singer singer = new Singer("Tanya", 15, 4);
+//
+//        //person.happyBirthday(); // Будет выполнен вариант класса Singer
+//
+//        //Person person = new Person("Petya", 8);
+//        //Singer singer = new Singer("Tanya", 15, 3);
+//
+//        System.out.println(checkPerson(singer));
 
-        //person.happyBirthday(); // Будет выполнен вариант класса Singer
+        Instrument[] instruments = {
+                new Guitar(),
+                new Piano(),
+                new Flute(),
+                new Guitar(),
+        };
 
-        //Person person = new Person("Petya", 8);
-        //Singer singer = new Singer("Tanya", 15, 3);
+        boolean[][] shouldPlay = {
+                {false, true, true, true, true, false},
+                {true, false, true, false, true, false},
+                {false, false, true, true, false, false},
+                {false, true, true, true, true, false},
+        };
 
-        System.out.println(checkPerson(singer));
+        Song song = new Song(instruments, shouldPlay);
+        song.play();
     }
 
-    public static boolean checkPerson(Person person) {
-        if (person.getAge() < 18) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+//    public static boolean checkPerson(Person person) {
+//        if (person.getAge() < 18) {
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
 }
