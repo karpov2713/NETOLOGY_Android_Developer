00c94bc84093dc8fb4433616de564c1800556150
karpov2713 <karpov2713@gmail.com>
2025-04-04 22:04:39 +0300

    Объект Comporator
    Сравнение объектов по различным параметрам

--- NET_OOP_Basics/NET_TreeMap_TreeSet/src/Main.java
+++ NET_OOP_Basics/NET_TreeMap_TreeSet/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("Petya", 8));
+        people.add(new Person("Tanya", 15));
+        people.add(new Person("Olya", 8));
+        people.add(new Person("Anya", 8));
+        System.out.println(people);
+
+        Collections.sort(people, new PersonsNameLengthComparator());
+        System.out.println(people);
+
     }
 }
